88c272c3bbf5a27fe7168498fd5367d2bfa78324
jojoin <yangjie@jojoin.com>
2020-10-15 21:37:53 +0800

    + scan value trsanfer rpc api

--- p2pv2/broadcast.go
+++ p2pv2/broadcast.go
@@ -4,10 +4,12 @@ package p2pv2
  * 广播消息
  */
 func (p *P2P) broadcastMessageToUnawarePeers(ty uint16, msgbody []byte, KnowledgeKind string, KnowledgeKey string) error {
+	var peers = []*Peer{}
+	for _, peer := range p.AllNodes {
+		peers = append(peers, peer)
+	}
 	go func() {
-		p.PeerChangeMux.Lock()
-		defer p.PeerChangeMux.Unlock()
-		for _, peer := range p.AllNodes {
+		for _, peer := range peers {
 			peer.SendUnawareMsg(ty, msgbody, KnowledgeKind, KnowledgeKey)
 		}
 	}()
