91a154310f9fbac0a644c84921595344149ccf54
jojoin <yangjie@jojoin.com>
2021-04-23 22:30:28 +0800

    + discoverNewBlock and transactionSubmit Mutex

--- handler/newtxblock.go
+++ handler/newtxblock.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hacash/core/blocks"
 	"github.com/hacash/core/interfaces"
 	"github.com/hacash/core/transactions"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -15,14 +16,27 @@ const (
 
 // 当前正在执行插入的区块高度
 var currentInsertingBlockHeight uint64 = 0
+var blockDiscoverMutex sync.Mutex
+var transactionSubmitMutex sync.Mutex
 
 func GetBlockDiscover(p2p interfaces.P2PManager, blockchain interfaces.BlockChain, peer interfaces.P2PMsgPeer, msgbody []byte) {
+	blockDiscoverMutex.Lock()
+	defer blockDiscoverMutex.Unlock()
+
 	//fmt.Println("GetBlockDiscover", 1)
 	// 先解析区块头，以提升并发收到区块时的性能
-	blockhead, _, e0 := blocks.ParseBlockHead(msgbody, 0)
+	blockhead, _, e0 := blocks.ParseExcludeTransactions(msgbody, 0)
 	if e0 != nil {
 		return // error end
 	}
+	//fmt.Println("GetBlockDiscover", 2)
+	blockhxstr := string(blockhead.Hash())
+	if p2p.CheckKnowledge("block", blockhxstr) {
+		return //
+	}
+	//fmt.Println("GetBlockDiscover", 3)
+	p2p.AddKnowledge("block", blockhxstr)
+	peer.AddKnowledge("block", blockhxstr)
 	curblkhei := blockhead.GetHeight()
 	// 判断正在插入的区块高度
 	if atomic.CompareAndSwapUint64(&currentInsertingBlockHeight, 0, curblkhei) {
@@ -45,14 +59,6 @@ func GetBlockDiscover(p2p interfaces.P2PManager, blockchain interfaces.BlockChai
 	if e1 != nil {
 		return // error end
 	}
-	//fmt.Println("GetBlockDiscover", 2)
-	blockhxstr := string(block.Hash())
-	if p2p.CheckKnowledge("block", blockhxstr) {
-		return //
-	}
-	//fmt.Println("GetBlockDiscover", 3)
-	p2p.AddKnowledge("block", blockhxstr)
-	peer.AddKnowledge("block", blockhxstr)
 	// insert block
 	//fmt.Println("GetBlockDiscover", 4)
 	//fmt.Println("get: MrklRoot", block.GetMrklRoot().ToHex(), hex.EncodeToString(msgbody), msgbody)
@@ -87,6 +93,9 @@ func GetBlockDiscover(p2p interfaces.P2PManager, blockchain interfaces.BlockChai
 }
 
 func GetTransactionSubmit(p2p interfaces.P2PManager, pool interfaces.TxPool, peer interfaces.P2PMsgPeer, msgbody []byte) {
+	transactionSubmitMutex.Lock()
+	defer transactionSubmitMutex.Unlock()
+
 	if pool == nil {
 		return // no pool
 	}
